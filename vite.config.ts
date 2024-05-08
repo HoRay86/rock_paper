e86ba1cd84d22ebb3e9fac5af68ee5bcd1882b9e
ray <rraywork1010@gmail.com>
2024-05-08 13:54:28 +0800

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/horay86.github.io/rock_paper/",
+  base: "/rock_paper/",
   plugins: [react()],
 });
